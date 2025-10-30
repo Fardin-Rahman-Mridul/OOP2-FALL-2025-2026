01ee47557ce25fd1666e12bcf1acd22517125a3a
Daud Ibrahim <daudibrahim029@gmail.com>
2025-10-30 12:45:13 +0600

    22-49760-3

---
+++ labTask1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+ 
+        Console.Write("Enter score of student 1: ");
+        double score1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter score of student 2: ");
+        double score2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter score of student 3: ");
+        double score3 = Convert.ToDouble(Console.ReadLine());
+
+
+        double average = (score1 + score2 + score3) / 3;
+        Console.WriteLine($"Average score = {average:F2}");
+    }
+}
