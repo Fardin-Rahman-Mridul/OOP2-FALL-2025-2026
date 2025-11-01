a297e78c2e3f880585ca23c41a913750543beb3d
Daud Ibrahim <daudibrahim029@gmail.com>
2025-11-01 14:13:46 +0600

    22-49760-3

---
+++ labTask1/labtask no 2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class LabTaskNo2
+{
+    public static void Main(string[] args)
+    {
+        double originalPrice = 250.0;
+        double discountRate = 15.0;
+
+        double discountAmount = (originalPrice * discountRate) / 100.0;
+        double finalPrice = originalPrice - discountAmount;
+
+        Console.WriteLine("Original Price: $" + originalPrice);
+        Console.WriteLine("Discount Rate: " + discountRate + "%");
+        Console.WriteLine("Discount Amount: $" + discountAmount);
+        Console.WriteLine("Final Price after Discount: $" + finalPrice);
+        Console.ReadLine();
+    }
+}
