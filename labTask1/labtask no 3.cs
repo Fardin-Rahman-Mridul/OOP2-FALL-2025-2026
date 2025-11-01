898d124bff7c20645d38a120d3e55550fb4729dd
Daud Ibrahim <daudibrahim029@gmail.com>
2025-11-01 14:20:17 +0600

    22-49760-3

---
+++ labTask1/labtask no 3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labTask1
+{
+    class labtask_no_3
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature in Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ labTask1/labtask no 4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labTask1
+{
+    class labtask_no_4
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.Write("Enter first number (a): ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number (b): ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("\nAfter swapping:");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            Console.ReadLine();
+        }
+    }
+}
