2ca887f43c67c9cf424e3317c88acab94bc627fb
Daud Ibrahim <daudibrahim029@gmail.com>
2025-12-15 19:26:16 +0600

    22-49760-3

---
+++ Lab/Multiple Interface Implementation/Multiple Interface Implementation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+interface IPrintable
+{
+    void Print();
+}
+
+interface ISerializable
+{
+    void Save();
+}
+
+class Report : IPrintable, ISerializable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing the report");
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("Saving the report");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Report rpt = new Report();
+        rpt.Print();
+        rpt.Save();
+    }
+}
---
+++ Lab/Student Record System/Student Record System/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+interface IStudent
+{
+    void Display();
+}
+
+class Student : IStudent
+{
+    private string name;
+    private int[] marks = new int[3];
+
+    public Student(string name)
+    {
+        this.name = name;
+    }
+
+    public int this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Student Name: " + name);
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine("Subject " + (i + 1) + " Marks: " + marks[i]);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student st = new Student("Rahim");
+
+        st[0] = 75;
+        st[1] = 82;
+        st[2] = 90;
+
+        st.Display();
+    }
+}
