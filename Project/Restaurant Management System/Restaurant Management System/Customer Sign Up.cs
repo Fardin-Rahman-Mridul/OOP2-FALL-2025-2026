a2c75a0077c935a41aab20e3b4a318f8a5599542
Daud Ibrahim <daudibrahim029@gmail.com>
2026-01-23 06:12:57 +0600

    22-49760-3

---
+++ Project/Restaurant Management System/Restaurant Management System/Customer Sign Up.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restaurant_Management_System
+{
+    public partial class Customer_Sign_Up : Form
+    {
+        public Customer_Sign_Up()
+        {
+            InitializeComponent();
+        }
+    }
+}
